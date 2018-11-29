958b4bbc9e1378befd6592cf294fe1b60760db09
Owen Thomas <owen@owenhthomas.com>
2018-11-28 18:14:21 -0800

    Quick test to make sure network service returns models

--- SwiftOverflow/ViewController.swift
+++ SwiftOverflow/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    let network = NetworkService()
+    network.getSwiftQuestions { (result, error) in
+      if let firstAnsweredQuestion = result?.questions.first(where: { $0.answers > 5 }) {
+        network.getAnswers(questionID: firstAnsweredQuestion.id) { (result, error) in
+          print(result?.answers.count)
+          print(result?.answers.first?.owner.name)
+        }
+      }
+      print(result?.questions.count)
+      print(result?.questions.first?.owner.name)
+    }
   }
-
-
 }
-
