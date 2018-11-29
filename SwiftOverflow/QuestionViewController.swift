fafdcdd34103db4a53e127d1d06ec896fa13f3c1
Owen Thomas <owen@owenhthomas.com>
2018-11-28 21:38:41 -0800

    Add simple question view controller

---
+++ SwiftOverflow/QuestionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionViewController.swift
+//  SwiftOverflow
+//
+//  Created by Owen Thomas on 11/28/18.
+//  Copyright © 2018 SwiftCoders. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+  @IBOutlet weak var tableView: UITableView!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
