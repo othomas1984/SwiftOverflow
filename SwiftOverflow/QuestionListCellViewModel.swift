36bbbbac5fa0d2e58ee35b9d1fb90c6b5e72c5b6
Owen Thomas <owen@owenhthomas.com>
2018-11-28 21:17:39 -0800

    Add question list cell view model

---
+++ SwiftOverflow/QuestionListCellViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  QuestionListCellViewModel.swift
+//  SwiftOverflow
+//
+//  Created by Owen Thomas on 11/28/18.
+//  Copyright © 2018 SwiftCoders. All rights reserved.
+//
+
+import Foundation
+import UIKit.UIImage
+
+class QuestionListCellViewModel {
+  private let network: NetworkService
+  private let question: Question
+  
+  init(question: Question, networkService: NetworkService) {
+    self.question = question
+    self.network = networkService
+  }
+  
+  var title: String {
+    return question.title
+  }
+  
+  var answers: String {
+    return "Answers: \(question.answerCount)"
+  }
+  
+  var authorName: String {
+    return question.owner.name
+  }
+  
+  func authorProfileImageData(completion: @escaping (Data?, Error?) -> Void) {
+    network.imageData(for: question.owner.imageURL, completion: completion)
+  }
+}
